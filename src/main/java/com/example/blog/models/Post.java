5cdbc2e365ddf09d75c0236808b5075817378915
KatherineSuprun <catesupr@gmail.com>
2024-05-15 16:10:07 +0300

    first

---
+++ src/main/java/com/example/blog/models/Post.java
@@ -0,0 +1,31 @@
+
+package com.example.blog.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title, anons, full_text;
+    private int views;
+
+    public Post() {
+    }
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
+}
+
+
